ca13cf1034da5c773722a830125dfd79c7ee25ae
Lucas Marques <lama@cesar.org.br>
2024-03-01 18:29:14 -0300

    feat: add eslint & create server

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(3000, () => console.log('🚀 Server started at http://localhost:3000'));
