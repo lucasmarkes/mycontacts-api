1f5fa4e078e0581f92f84a662bf6df398508111c
Lucas Marques <lama@cesar.org.br>
2024-03-01 19:19:46 -0300

    feat: created Conct controller/ routes file

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,24 @@
+class ContactController {
+  index() {
+    // List all of registers
+  }
+
+  show() {
+    // Show a single register
+  }
+
+  store() {
+    // Create a new register
+  }
+
+  update() {
+    // Update a register
+  }
+
+  delete() {
+    // Delete a register
+  }
+}
+
+// Singleton Pattern
+module.exports = new ContactController();
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 
-const app = express();
+const routes = require('./routes');
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+const app = express();
+app.use(routes);
 
 app.listen(3000, () => console.log('🚀 Server started at http://localhost:3000'));
